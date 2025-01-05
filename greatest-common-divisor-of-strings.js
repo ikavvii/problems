5ab68cafd95b3f90d3bac871e8d3d5dbff57b24d
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-05 23:47:30 +0530

    Using the actual GCD function 🤯

--- greatest-common-divisor-of-strings.js
+++ greatest-common-divisor-of-strings.js
@@ -5,6 +5,23 @@
  */
 
 var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+    return "";
+  }
+
+  function gcd(a, b) {
+    while (b != 0) {
+      let temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+
+  return str1.slice(0, gcd(str1.length, str2.length));
+};
+
+var gcdOfStrings1 = function (str1, str2) {
   let shortString, longString;
 
   if (str1.length > str2.length) {
@@ -15,32 +32,29 @@ var gcdOfStrings = function (str1, str2) {
     shortString = str1;
   }
 
-  let gcdLengthPossible = longString.length / 2;
-  //   console.log(gcdLengthPossible);
+  let gcdLengthPossible = longString.length;
 
-  let gcd = "";
+  let gcd = "",
+    GCD = "";
 
   let isGCD = false;
 
-  while (gcdLengthPossible > 0 && !isGCD) {
-    for (let i = 0; i < shortString.length; i++) {
-      if (gcd.length <= gcdLengthPossible) {
-        if (str1[i] === str2[i]) {
-          gcd += str1[i];
-        } else {
-          gcd = "";
+  for (let i = 0; i < shortString.length; i++) {
+    if (gcd.length <= gcdLengthPossible) {
+      if (str1[i] === str2[i]) {
+        gcd += str1[i];
+        isGCD =
+          longString.split(gcd).every((c) => c === "") &&
+          shortString.split(gcd).every((c) => c === "");
+        if (isGCD) {
+          GCD = gcd;
         }
+      } else {
+        return "";
       }
     }
-    isGCD = longString.split(gcd).every((c) => c === "");
-
-    if (!isGCD) {
-      gcdLengthPossible--;
-      gcd = ''
-    }
   }
-
-  return isGCD ? gcd : "";
+  return GCD;
 };
 
 console.log(gcdOfStrings("ABCABC", "ABC"));
@@ -52,4 +66,11 @@ console.log(
     "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
   )
 );
-
+console.log(gcdOfStrings("ABCDEF", "ABC"));
+console.log(gcdOfStrings("ABABABAB", "ABAB"));
+console.log(
+  gcdOfStrings(
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  )
+);
